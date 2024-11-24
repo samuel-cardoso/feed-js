3cf427cf3403a0557acaf6cfcdaf0064908682a6
Samuel <samuelcardosodev@gmail.com>
2024-11-24 17:12:45 -0300

    feat: create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { Header } from "./components/Header"
+import { Post } from "./Post"
+import Sidebar from "./components/Sidebar"
+
+import styles from "./App.module.css"
 import "./global.css"
 
 function App() {
 
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Samuel Cardoso da Silva"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies, libero eget ultricies elementum, lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies, libero eget ultricies elementum."
+          />
+          <Post 
+            author="Samuel Cardoso da Silva"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies, libero eget ultricies elementum, lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies, libero eget ultricies elementum."
+          />
+        </main>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import style from './Sidebar.module.css';
+
+export default function Sidebar() {
+  return (
+    <aside className={style.sidebar} >
+      <img className={style.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    
+    <div className={style.profile}>
+        <strong>Samuel Cardoso da Silva</strong>
+        <span>Web Developer</span>
+    </div>
+
+    <footer>
+        <a href="#">
+            Editar seu perfil
+        </a>
+    </footer>
+    </aside>
+  )
+}
