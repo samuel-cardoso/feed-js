8455f2fa50a9613667769487ba5e711086ab4294
Samuel <samuelcardosodev@gmail.com>
2024-11-25 22:17:24 -0300

    feat: finish sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import style from './Sidebar.module.css';
 
 export default function Sidebar() {
@@ -6,13 +8,15 @@ export default function Sidebar() {
       <img className={style.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
     
     <div className={style.profile}>
+        <img className={style.avatar} src="https://github.com/samuel-cardoso.png" />
         <strong>Samuel Cardoso da Silva</strong>
         <span>Web Developer</span>
     </div>
 
     <footer>
         <a href="#">
-            Editar seu perfil
+        <PencilLine size={20} />
+        Editar seu perfil
         </a>
     </footer>
     </aside>
