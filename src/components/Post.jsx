eb82a7f586c0e08075d74a31b53064ed7f8db6ce
Samuel <samuelcardosodev@gmail.com>
2025-01-07 07:45:03 -0300

    feat: applying form validation in the textarea field with changing the behavior of the publication button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -16,6 +16,8 @@ export function Post({ author, publishedAt, content }) {
 
     const [newCommentText, setNewCommentText] = useState('');
 
+    console.log(newCommentText); // A cada digitação feita a contece uma renderização do meu componente Post. Por isso que a cada dígito o react dispara um novo console.log.
+
     // Usando Inrl para formatar a data.
     // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
     //     day: '2-digit',
@@ -41,8 +43,15 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+        console.log(event);
+    }
+
 
     function deleteComment(commentToDelete) {
         const commentsWithoutDeletedOne = comments.filter(comment => {
@@ -51,6 +60,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
        return (
         <article className={styles.post}>
@@ -84,10 +95,12 @@ export function Post({ author, publishedAt, content }) {
                     name='comment'
                     placeholder="Deixe um comentário" 
                     value={newCommentText}
-                    onChange={handleNewCommentChange}   
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required   
                 />
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
