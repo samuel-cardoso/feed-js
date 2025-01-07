5d60e8eade99e296a82240588cd940ed3ed94bcf
Samuel <samuelcardosodev@gmail.com>
2025-01-07 07:14:56 -0300

    feat: insert functionality for delete commentary

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/samuel-cardoso.png" alt="" />
@@ -14,7 +19,7 @@ export function Comment({ content }) {
                             <time title="26 de Novembro às 21:30h" dateTime='2024-11-26 21:31:00'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,13 +36,20 @@ export function Post({ author, publishedAt, content }) {
     function handleCreateNewComment() {
         event.preventDefault()
         
-        // Imutabilidade
         setComments([...comments, newCommentText])
         setNewCommentText('');
     }
 
     function handleNewCommentChange() {
         setNewCommentText(event.target.value);
+    }
+
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+
+        setComments(commentsWithoutDeletedOne);
     }
 
        return (
@@ -86,7 +93,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment} />
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
                 })}
             </div>
 
